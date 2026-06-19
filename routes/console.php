ed8244947addab3e23755e93d3b5ffe2a75d5881
Joshua M <91457812+JoshuaMicallefYBSU@users.noreply.github.com>
2026-06-19 15:08:33 +1000

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Schedule;
 use App\Jobs\VATPAC\ATCSessions;
+use App\Jobs\RCL\OperationsCenter;
 
 // Every Minute
 // Schedule::job(new ATCSessions)->everyMinute();
+Schedule::job(new OperationsCenter)->everyMinute();
